72e41604ac85fabcd3e65579753e37241fb22aa2
Phil Kulin <schors@gmail.com>
2019-12-01 23:15:42 +0300

    split some files

--- main.go
+++ main.go
@@ -7,7 +7,6 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
-	"regexp"
 
 	tb "github.com/go-telegram-bot-api/telegram-bot-api"
 
@@ -42,70 +41,6 @@ func GetUpdatesChan(bot *tb.BotAPI) <-chan tb.Update {
 		log.Panic(err)
 	}
 	return updates
-}
-
-// Handle Chat message
-func Talks(c pb.CheckClient, bot *tb.BotAPI, update tb.Update) {
-	// who writing
-	UserName := ""
-	if update.Message.From != nil {
-		UserName = update.Message.From.UserName
-	}
-	// ID of chat/dialog
-	// maiby eq UserID or public chat
-	ChatID := update.Message.Chat.ID
-	bot.Send(tb.NewChatAction(ChatID, "typing"))
-	// message text
-	Text := update.Message.Text
-	//log.Printf("[%s] %d %s", UserName, ChatID, Text)
-	regex, _ := regexp.Compile(`^/([A-Za-z\_]+)\s*(.*)$`)
-	matches := regex.FindStringSubmatch(Text)
-	// hanlde chat commands
-	if len(matches) > 0 {
-		var reply string
-		comm := matches[1]
-		commArgs := []string{""}
-		if len(matches) >= 3 {
-			commArgs = regexp.MustCompile(`\s+`).Split(matches[2], -1)
-		}
-		switch comm {
-		case `help`:
-			reply = HelpMessage
-		case `helpru`:
-			reply = HelpMessageRu
-		case `donate`:
-			reply = DonateMessage
-		case `ck`, `check`:
-			if len(commArgs) > 0 {
-				reply = mainSearch(c, commArgs[len(commArgs)-1])
-			} else {
-				reply = "😱 Noting to search\n"
-			}
-		case `start`:
-			reply = "Glad to see you, " + UserName + "!\n"
-		//case `ping`:
-		//	reply = Ping(c)
-		default:
-			reply = "😱 Unknown command\n"
-		}
-		if reply != `` {
-			msg := tb.NewMessage(ChatID, reply+Footer)
-			msg.ParseMode = tb.ModeMarkdown
-			msg.DisableWebPagePreview = true
-			_, err := bot.Send(msg)
-			if err != nil {
-				Warning.Printf("Error sending message: %s\n", err.Error())
-			}
-		}
-	} else {
-		msg := tb.NewMessage(ChatID, mainSearch(c, Text)+Footer)
-		msg.ParseMode = tb.ModeMarkdown
-		msg.DisableWebPagePreview = true
-		_, err := bot.Send(msg)
-		if err != nil {
-			Warning.Printf("Error sending message: %s\n", err.Error())
-		}
-	}
 }
 
 func main() {
