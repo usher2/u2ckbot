09f4fd2297b6c9e8db1ddaaa4012f99a27fe9b7e
Phil Kulin <schors@gmail.com>
2020-02-23 02:41:36 +0300

    Add decision's basis to answer

--- answers.go
+++ answers.go
@@ -50,6 +50,28 @@ func printUpToDate(t int64) string {
 	return fmt.Sprintf("\n%c _The data was synced not earlier than:_ %s\n", r, time.Unix(t, 0).In(time.FixedZone("UTC+3", 3*60*60)).Format(time.RFC3339))
 }
 
+func constructBasis(content *TContent) (res string) {
+	basis := "[статья 15.1](http://www.consultant.ru/document/cons_doc_LAW_61798/38c8ea666d27d9dc12b078c556e316e90248f551/), общая"
+	if content.EntryType == 1 && content.Decision.Org == "Генпрокуратура" {
+		basis = "[статья 15.1-1](http://www.consultant.ru/document/cons_doc_LAW_61798/079aac275ffc6cea954b19c5b177a547b94f3c48/), неуважение"
+	} else if content.EntryType == 2 {
+		basis = "[статья 15.2](http://www.consultant.ru/document/cons_doc_LAW_61798/1f316dc4a18023edcd030bc6591c4dd8b4f841dc/), правообладание"
+	} else if content.EntryType == 3 {
+		basis = "[статья 15.3](http://www.consultant.ru/document/cons_doc_LAW_61798/34547c9b6ddb60cebd0a67593943fd9ef64ebdd0/), мятеж и фейки"
+	} else if content.EntryType == 4 {
+		basis = "[статья 15.4](http://www.consultant.ru/document/cons_doc_LAW_61798/96723dcd9be73473a978013263f16f42cd8cd53d/), ОРИ"
+	} else if content.EntryType == 5 && content.Decision.Org == "Мосгорсуд" {
+		basis = "[статьи 15.6](http://www.consultant.ru/document/cons_doc_LAW_61798/c7c4ad36689c46c7e8a3ab49c9db8ccbc7c82920/), вечно"
+	} else if content.EntryType == 5 && content.Decision.Org == "Минкомсвязь" {
+		basis = "[статьи 15.6-1](http://www.consultant.ru/document/cons_doc_LAW_61798/c7c4ad36689c46c7e8a3ab49c9db8ccbc7c82920/), вечно"
+	} else if content.EntryType == 6 {
+		basis = "[статьи 15.5](http://www.consultant.ru/document/cons_doc_LAW_61798/98228cbe6565abbe55d0842a7e8593012c3449ea/), ПД"
+	} else if content.EntryType == 7 {
+		basis = "[статьи 15.8](http://www.consultant.ru/document/cons_doc_LAW_61798/1a807328c80a540bd0bb724927d6e774595431dc/), VPN"
+	}
+	return basis
+}
+
 func constructContentResult(a []*pb.Content) (res string) {
 	var mass string
 	var oldest int64 = MAX_TIMESTAMP
@@ -84,7 +106,8 @@ func constructContentResult(a []*pb.Content) (res string) {
 			bt = "\u274c (ip) "
 		}
 		dcs := fmt.Sprintf("%s %s %s", content.Decision.Org, content.Decision.Number, content.Decision.Date)
-		res += fmt.Sprintf("%s /n\\_%d %s\n", bt, content.Id, dcs)
+		basis := constructBasis(&content)
+		res += fmt.Sprintf("%s /n\\_%d %s (%s)\n", bt, content.Id, dcs, basis)
 		res += fmt.Sprintf("included: %s\n", time.Unix(content.IncludeTime, 0).In(time.FixedZone("UTC+3", 3*60*60)).Format(time.RFC3339))
 		for i, d := range content.Domain {
 			if i >= PRINT_LIMIT {
@@ -271,7 +294,8 @@ func constructResult(a []*pb.Content) (res string) {
 				cbi++
 			}
 			dcs := fmt.Sprintf("%s %s %s", content.Decision.Org, content.Decision.Number, content.Decision.Date)
-			res += fmt.Sprintf("%s /n\\_%d %s\n", bt, content.Id, dcs)
+			basis := constructBasis(&content)
+			res += fmt.Sprintf("%s /n\\_%d %s (%s)\n", bt, content.Id, dcs, basis)
 			if len(req.Aggr) != 0 {
 				for _, nw := range req.Aggr {
 					res += fmt.Sprintf("    _as subnet_ %s\n", nw)
--- go.mod
+++ go.mod
@@ -4,13 +4,13 @@ go 1.13
 
 require (
 	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible
-	github.com/golang/protobuf v1.3.2
+	github.com/golang/protobuf v1.3.3
 	github.com/miekg/dns v1.1.27
 	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
-	golang.org/x/crypto v0.0.0-20200115085410-6d4e4cb37c7d // indirect
-	golang.org/x/net v0.0.0-20200114155413-6afb5195e5aa
-	golang.org/x/sys v0.0.0-20200116001909-b77594299b42 // indirect
+	golang.org/x/crypto v0.0.0-20200221231518-2aa609cf4a9d // indirect
+	golang.org/x/net v0.0.0-20200222125558-5a598a2470a0
+	golang.org/x/sys v0.0.0-20200219091948-cb0a6d8edb6c // indirect
 	golang.org/x/text v0.3.2 // indirect
-	google.golang.org/genproto v0.0.0-20200115191322-ca5a22157cba // indirect
-	google.golang.org/grpc v1.26.0
+	google.golang.org/genproto v0.0.0-20200218151345-dad8c97a84f5 // indirect
+	google.golang.org/grpc v1.27.1
 )
