da39c022a6a50be4c0d71a38360f748a149a0c17
Phil Kulin <schors@gmail.com>
2023-05-14 01:58:17 +0300

    fix: fix sanitize

--- sanitize.go
+++ sanitize.go
@@ -1,8 +1,7 @@
 package main
 
 import (
-	"strings"
-
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"golang.org/x/net/idna"
 )
 
@@ -15,8 +14,8 @@ func PrintedDomain(domain string) string {
 }
 
 func Sanitize(s string) (res string) {
-	res = strings.ReplaceAll(s, "_", "\\_")
-	res = strings.ReplaceAll(res, "*", "\\*")
-	res = strings.ReplaceAll(res, "`", "\\`")
-	return
+	// res = strings.ReplaceAll(s, "_", "\\_")
+	// res = strings.ReplaceAll(res, "*", "\\*")
+	// res = strings.ReplaceAll(res, "`", "\\`")
+	return tgbotapi.EscapeText(tgbotapi.ModeMarkdown, s)
 }
